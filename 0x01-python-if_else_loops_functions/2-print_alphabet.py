ea7399d86ac62538269d86ffc315a0e6292f4d9d
PenelopeSuke <penelopesuke4@gmail.com>
2023-01-04 19:35:48 -0800

    Task 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
+#2-print_alphabet.py
 """print  alphabet, in lowercase, not followed by a new line.""" 
 for a in range(97, 123):
     print("{}".format(chr(a)), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for a in range(97, 123):
+    if chr(a) is not 'q' and chr(a) is not 'e':
+        print("{}".format(chr(a)), end="")
