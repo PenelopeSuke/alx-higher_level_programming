7fe58cf4441cf30a4de3edc0535b9f75c0867bca
PenelopeSuke <penelopesuke4@gmail.com>
2023-01-05 22:20:08 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -10,6 +10,6 @@ while dig1 <= 8:
                  print("{:d}, ".format(dig2), end='')
              else:
                   print("{:d}".format(dig2))
-                   dig2 += 1
-                   dig2 = dig1 + 1
-                   dig1 = dig1 + 1
+                  dig2 += 1
+                dig2 = dig1 + 1
+                dig1 = dig1 + 1
