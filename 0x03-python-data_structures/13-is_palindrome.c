9c6cc1ba995a35fa9c4214bf4be445fae24dae73
PenelopeSuke <penelopesuke4@gmail.com>
2023-01-08 13:47:16 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,31 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0
+ * comparator - compare start and end position
+ * @head: the linked list
+ * @last: the end of the list
+ * Return: 1 true, 0 false
+ */
+int comparator(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (comparator(head, last->next) && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: the linked list
+ * Return: 1 true, 0 false
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int cur = 1;
-	listint_t *tem;
-
-	if (head == NULL || *head == NULL)
+	if (!head || !(*head))
 		return (1);
-
-	tem = *head;
-	while (tem)
-	{
-		tem = tem->next;
-		cur++;
-	}
-	return (0);
+	return (comparator(head, *head));
 }
