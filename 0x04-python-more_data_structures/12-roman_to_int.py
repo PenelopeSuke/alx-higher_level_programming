9274499026dc063adb2cc6dc98130a6d599c5a24
PenelopeSuke <penelopesuke4@gmail.com>
2023-01-13 08:40:16 -0800

    Task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    r = 0
-    s = 0
-
-    if type(roman_string) is str and roman_string:
-        for c in range(len(roman_string) - 1, -1, -1):
-            if val[roman_string[c]] >= s:
-                r += val[roman_string[c]]
+    if type(roman_string) is not str or roman_string is None:
+        return (0)
+    sum = 0
+    r_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for idx in range(len(roman_string)):
+        i = r_dic.get(roman_string[idx])
+        if (idx < len(roman_string) - 1):
+            ii = r_dic.get(roman_string[idx + 1])
+            if ii > i:
+                sum -= i
             else:
-                r -= val[roman_string[c]]
-                s = val[roman_string[c]]
-                return r
+                sum += i
+        else:
+                sum += i
+    return (sum)
